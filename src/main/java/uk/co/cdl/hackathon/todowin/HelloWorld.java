aaf584eac674801fabdaddf7d3f1a50e38354cac
AK <ananyaa.kumar@cdl.co.uk>
2017-03-31 13:35:36 +0100

    test commit
    Change-Id: Ia93fcdd3ea12d2fdfce201d00b6ea97fc375c436

---
+++ src/main/java/uk/co/cdl/hackathon/todowin/HelloWorld.java
@@ -0,0 +1,11 @@
+package uk.co.cdl.hackathon.todowin;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("YoYo.");
+		new ArrayList<String>().stream().filter(String::isEmpty);
+	}
+}
