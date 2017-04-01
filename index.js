7eb078069511e147714107c8b48af442aba6b80f
robertma <robert.march@cdl.co.uk>
2017-04-01 16:19:12 +0100

    A few more tweaks

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 var States = {
     Start: "_StartState",
+    DeviceSelection: "_DeviceSelection",
     Quote: "_QuoteState",
     Payment: "_PaymentState",
+    AmazonPayMethod: "_AmazonPayMethod",
     VisaAccNo: "_VisaAccountNumberState",
     VisaSortCode: "_VisaSortCodeState",
     VisaCVC: "_VisaCVCState",
@@ -15,13 +17,15 @@ var States = {
 var languageString = {
     "en": {
         "translation": {
-            "WELCOME_MESSAGE": "Welcome to Auto Insurer. Do you want to insure your device?",
+            "WELCOME_MESSAGE": "Welcome to Auto Insurer. Do you want to automatically insure one of your devices?",
             "START_UNHANDLED": "Welcome to Auto Insurer. You can ask me to automatically insure any of your connected devices.",
             "STOP_MESSAGE": "Are you sure you'd like to stop?",
             "CANCEL_MESSAGE": "Cool, laters bro",
+            "DEVICE_SELECTION_MESSAGE": "Which of the following devices do you want to insure? Samsung Galaxy S5 Neo. Apple iPhone 7.",
             "QUOTE_MESSAGE": "Ok. I've found a quote for 13 pounds 72 pence per year. Would you like to proceed with this?",
-            "PAYMENT_MESSAGE": "You can pay by either Visa or PayPal. How would you like to pay?",
-            "PAYMENT_METHOD_UNRECOGNISED_MESSAGE": "I'm sorry, I didn't understand that.",
+            "PAYMENT_MESSAGE": "You can pay using a card linked to your amazon account, or by Visa or PayPal. How would you like to pay?",
+            "PAYMENT_METHOD_UNRECOGNISED_MESSAGE": "I'm sorry, I didn't understand that. ",
+            "PAY_BY_AMAZON_ACCOUNT_MESSAGE": "Are you happy to proceed to pay through your Amazon Account?",
             "PAY_BY_VISA_MESSAGE_CARD_NO": "To pay using a Visa credit, or debit card, please tell me your card number.",
             "PAY_BY_VISA_MESSAGE_SORT_CODE": "What is your sort code?",
             "PAY_BY_VISA_MESSAGE_CVC": "What is your card security code? This is the last three digits of the number on the back of your card.",
@@ -46,7 +50,7 @@ exports.handler = function(event, context, callback) {
                 this.handler.state = States.Start;
                 this.emitWithState("StartRiskCapture");
             },
-            "Unhandled": function () {
+            "Unhandled": function () { // See https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs/issues/43
                 this.emit(":tell", this.t("START_UNHANDLED"));
             }
         },
@@ -58,13 +62,34 @@ exports.handler = function(event, context, callback) {
                     this.emit(":askWithCard", speechOutput, speechOutput, speechOutput, speechOutput);
                 },
                 "AMAZON.YesIntent": function() {
-                    this.handler.state = States.Quote;
-                    this.emitWithState("Quote");
+                    this.handler.state = States.DeviceSelection;
+                    this.emitWithState("DeviceSelection");
                 },
                 "AMAZON.NoIntent": function() {
                     this.emit(":tell", this.t("CANCEL_MESSAGE"));
                 }
            });
+        },
+
+        deviceSelectionHandlers: function() {
+            return Alexa.CreateStateHandler(States.DeviceSelection, {
+                "DeviceSelection": function() {
+                    var speechOutput = this.t("DEVICE_SELECTION_MESSAGE");
+                    this.emit(":askWithCard", speechOutput, speechOutput, speechOutput, speechOutput);
+                },
+                "Samsung": function() {
+                    this.handler.state = States.Quote;
+                    this.emitWithState("Quote");
+                },
+                "IPhone": function() {
+                    this.handler.state = States.Quote;
+                    this.emitWithState("Quote");
+                },
+                "Unhandled": function() {
+                    var speechOutput = this.t("PAYMENT_METHOD_UNRECOGNISED_MESSAGE") + this.t("DEVICE_SELECTION_MESSAGE");
+                    this.emit(":askWithCard", speechOutput, speechOutput, speechOutput, speechOutput);
+                }
+            });
         },
 
         quoteStateHandlers: function() {
@@ -89,6 +114,10 @@ exports.handler = function(event, context, callback) {
                     var speechOutput = this.t("PAYMENT_MESSAGE");
                     this.emit(":askWithCard", speechOutput, speechOutput, speechOutput, speechOutput);
                 },
+                "AmazonAccountPayMethod": function() {
+                    this.handler.state = States.AmazonPayMethod;
+                    this.emitWithState("AmazonPayMethod");
+                },
                 "VisaPayMethod": function() {
                     this.handler.state = States.VisaAccNo;
                     this.emitWithState("VisaAccountNumber");
@@ -101,6 +130,22 @@ exports.handler = function(event, context, callback) {
                     var speechOutput = this.t("PAYMENT_METHOD_UNRECOGNISED_MESSAGE") + this.t("PAYMENT_MESSAGE");
                     this.emit(":askWithCard", speechOutput, speechOutput, speechOutput, speechOutput);
                 },
+            });
+        },
+
+        amazonPayMethodHandlers: function() {
+            return Alexa.CreateStateHandler(States.AmazonPayMethod, {
+                "AmazonPayMethod": function() {
+                    var speechOutput = this.t("PAY_BY_AMAZON_ACCOUNT_MESSAGE");
+                    this.emit(":askWithCard", speechOutput, speechOutput, speechOutput, speechOutput);
+                },
+                "AMAZON.YesIntent": function() {
+                    this.handler.state = States.Confirmation;
+                    this.emitWithState("Confirmation");
+                },
+                "AMAZON.NoIntent": function() {
+                    this.emit(":tell", this.t("CANCEL_MESSAGE"));
+                }
             });
         },
 
@@ -172,8 +217,10 @@ exports.handler = function(event, context, callback) {
     alexa.registerHandlers(
         AutoInsurer.newSessionHandlers,
         AutoInsurer.startStateHandlers(),
+        AutoInsurer.deviceSelectionHandlers(),
         AutoInsurer.quoteStateHandlers(),
         AutoInsurer.paymentStateHandlers(),
+        AutoInsurer.amazonPayMethodHandlers(),
         AutoInsurer.visaAccNoStateHandlers(),
         AutoInsurer.visaSortCodeStateHandlers(),
         AutoInsurer.visaCVCStateHandlers(),
